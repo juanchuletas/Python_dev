6aa79200fa99dcd1babda5c75903e3c0e851dcda
juanchuletas <gamj@xanum.uam.mx>
2019-05-20 00:29:25 -0500

    First commit

---
+++ function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+def main():
+    print("Exploit.. test..")
+    runExploit()
+def runExploit():
+    print("I'm the exploit")
+if __name__ == "__main__":
+    main()
---
+++ indentation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+
+#coding=utf-8
+def main():
+    print('A line inside the main function...')
+print("A line outside main function...")
+if __name__ == main():main()
